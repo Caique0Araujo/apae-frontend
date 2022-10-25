eaee3a1bc7ad4ac1eb9928638674176f4e53badd
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-25 20:37:00 -0300

    Updated style of bazar

--- src/pages/bazar/bazar.jsx
+++ src/pages/bazar/bazar.jsx
@@ -1,9 +1,29 @@
+import { Col, Container, Row } from "react-bootstrap";
+import ButtonComponent from "../components/button-component/button-component";
 import MenuBarComponent from "../components/menu-bar-component/menu-bar-component";
+import OutlinedButtonComponent from "../components/outlined-button-component/outlined-button-component";
 
 function Bazar() {
     return (
         <div>
             <MenuBarComponent/>
+
+            <Container style={{marginTop: 32, marginBottom: 100}}>
+                <Row>
+                    <Col>
+                        ola
+                    </Col>
+                    <Col>
+                        <h2 style={{fontSize: 75, textAlign: 'right', color: '#666666'}}>Conheça o bazar da APAE</h2>
+                        <p style={{fontSize: 16, textAlign: 'right', color: '#666666', marginBlock: 28}}>Falar como funciona o bazar, como começou talvez. Falar como doar e como comprar. Accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In </p>
+                    
+                        <Row style={{justifyContent: 'space-between'}}>
+                            <OutlinedButtonComponent text='Doar'/>
+                            <ButtonComponent text='Comprar'/>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
