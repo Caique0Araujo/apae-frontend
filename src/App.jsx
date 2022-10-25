8f5d7c33900da0bf171c8b244844bd7f60ca0b68
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-25 20:37:08 -0300

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Bazar from './pages/bazar/bazar';
+import './app.css';
 
 function App() {
   return (
     <div className="App">
+      <Bazar/>
     </div>
   )
 }
