c2c782cddc163f0e59349a4a04a5c3479878616e
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-25 19:48:27 -0300

    Renamed shop to bazar

---
+++ src/pages/bazar/bazar.jsx
@@ -0,0 +1,11 @@
+import MenuBarComponent from "../components/menu-bar-component/menu-bar-component";
+
+function Bazar() {
+    return (
+        <div>
+            <MenuBarComponent/>
+        </div>
+    );
+}
+
+export default Bazar;
