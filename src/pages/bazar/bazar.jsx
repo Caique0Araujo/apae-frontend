ef816d6ac5e8ee8f652498cb73b341f4ff25a33b
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-26 11:43:03 -0300

    Added texts

--- src/pages/bazar/bazar.jsx
+++ src/pages/bazar/bazar.jsx
@@ -26,6 +26,32 @@ function Bazar() {
                     </Col>
                 </Row>
             </Container>
+
+            <Container>
+                <Row style={{justifyContent: 'space-between'}}>
+                    <Col xs={4}>
+                        <h4 style={{fontSize: 18, color: '#666666'}}>Como doar algo para o bazar?</h4>
+                        <p style={{fontSize: 14, color: '#666666'}}>Dizer aqui como deve ser feita a doação.<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
+                    </Col>
+
+                    <Col xs={4}>
+                        <h4 style={{textAlign: 'right', fontSize: 18, color: '#666666'}}>O que posso doar?</h4>
+                        <p style={{textAlign: 'right', fontSize: 14, color: '#666666'}}>Dizer aqui quais tipos de coisas podem ser doadas.<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
+                    </Col>
+                </Row>
+
+                <Row style={{justifyContent: 'space-between'}}>
+                    <Col xs={4}>
+                        <h4 style={{fontSize: 18, color: '#666666'}}>Qual o local para fazer a doação?</h4>
+                        <p style={{fontSize: 14, color: '#666666'}}>Dizer aqui o local para fazer a doação<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
+                    </Col>
+
+                    <Col xs={4}>
+                        <h4 style={{textAlign: 'right', fontSize: 18, color: '#666666'}}>Qualquer outra informação relevante</h4>
+                        <p style={{textAlign: 'right', fontSize: 14, color: '#666666'}}>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
