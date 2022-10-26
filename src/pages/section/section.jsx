5c30238d34b3a9658100b64757f44226dfa60456
Caique0Araujo <caique.ara19@gmail.com>
2022-10-26 09:39:32 -0300

    fix: adjust row width to fill screen

--- src/pages/section/section.jsx
+++ src/pages/section/section.jsx
@@ -1,5 +1,6 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Image} from 'react-bootstrap';
 import sectionImage from '../../../public/health-section.svg';
+import MenuBarComponent from '../components/menu-bar-component/menu-bar-component';
 
 const subsection = ['psicologia', 'fisioterapia'];
 
@@ -7,17 +8,16 @@ const subsection = ['psicologia', 'fisioterapia'];
 const Section = (props) => {
   return (
     <>
-      
-      <Container style={{margin: 0, }}>
-        <Row style={{padding: 0}}>
-          <Col xs={14} md={9} style={{marginLeft: '7%', padding: 20, backgroundImage: "linear-gradient(to right, #FFFFFF, #DFE9E8)", display: 'flex', alignItems: 'center'}} >
+      <MenuBarComponent/>
+      <Container style={{margin: 0, padding: 0}} fluid>
+        <Row style={{padding: 0, width: '100%', margin: 0}}>
+          <Col style={{ width: '70%', marginLeft: '7%', padding: 20, backgroundImage: "linear-gradient(to right, #FFFFFF, #DFE9E8)", display: 'flex', alignItems: 'center'}} >
           <h1 style={{color: "rgba(102, 102, 102, 1) "}}>
             Setor de {props.name} da APAE SJE
           </h1>
           </Col>
-          <Col xs={2} md={1} style={{padding: 0}}>
-            <img src={sectionImage} style={{left: 0 }}/>
-            
+          <Col style={{padding: 0, width: '30%'}}>
+            <Image src={sectionImage} fluid style={{width: '100%'}}/>           
           </Col>
         </Row>
         {subsection.map((section) => {
