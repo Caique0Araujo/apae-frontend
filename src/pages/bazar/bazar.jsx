8658969b1d1dfde96539133621140ca66c80085d
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-26 11:15:47 -0300

    Updated bazar style

--- src/pages/bazar/bazar.jsx
+++ src/pages/bazar/bazar.jsx
@@ -2,20 +2,21 @@ import { Col, Container, Row } from "react-bootstrap";
 import ButtonComponent from "../components/button-component/button-component";
 import MenuBarComponent from "../components/menu-bar-component/menu-bar-component";
 import OutlinedButtonComponent from "../components/outlined-button-component/outlined-button-component";
+import bazar from '../../assets/images/bazar.png';
 
 function Bazar() {
     return (
         <div>
             <MenuBarComponent/>
 
-            <Container style={{marginTop: 32, marginBottom: 100}}>
-                <Row>
+            <Container>
+                <Row style={{marginTop: 32, marginBottom: 100}}>
                     <Col>
-                        ola
+                        <img src={bazar} height={450}/>
                     </Col>
-                    <Col>
-                        <h2 style={{fontSize: 75, textAlign: 'right', color: '#666666'}}>Conheça o bazar da APAE</h2>
-                        <p style={{fontSize: 16, textAlign: 'right', color: '#666666', marginBlock: 28}}>Falar como funciona o bazar, como começou talvez. Falar como doar e como comprar. Accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In </p>
+                    <Col style={{marginTop: 75}}>
+                        <h2 style={{fontSize: 65, textAlign: 'right', color: '#666666', fontWeight: 500}}>Conheça o bazar da APAE</h2>
+                        <p style={{fontSize: 16, textAlign: 'right', color: '#666666', marginBlock: 28, fontWeight: 300}}>Falar como funciona o bazar, como começou talvez. Falar como doar e como comprar. Accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In </p>
                     
                         <Row style={{justifyContent: 'space-between'}}>
                             <OutlinedButtonComponent text='Doar'/>
