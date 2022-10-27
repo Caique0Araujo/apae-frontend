8a85e32aa0ebc35632ce8d5bc40a5faf68547d25
Caique0Araujo <caique.ara19@gmail.com>
2022-10-26 21:22:25 -0300

    feat: add subsections to section screen

--- src/pages/section/section.jsx
+++ src/pages/section/section.jsx
@@ -1,13 +1,14 @@
 import {Container, Row, Col, Image} from 'react-bootstrap';
 import sectionImage from '../../assets/images/health-section.svg';
 import MenuBarComponent from '../components/menu-bar-component/menu-bar-component';
+import psychoImage from '../../assets/images/health-section-psycho.png';
 
 
 const psychology = {
   title: 'Psicologia',
-  subtitle: '',
-  text: '',
-  image: ''
+  subtitle: 'Algo sobre isto para servir como subtítulo. (essas imagens são apenas ilustrativas, podendo ser trocadas)',
+  text: 'Um parágrafo maior aqui a respeito do serviço em questão. eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. Sed in est lacus. Integer ex erat, accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In ut eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. Sed in est lacus. Integer ex erat, accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In ut ',
+  image: psychoImage
 }
 
 const subsection = [psychology];
@@ -18,20 +19,41 @@ const Section = (props) => {
     <>
       <MenuBarComponent/>
       <Container style={{margin: 0, padding: 0}} fluid>
-        <Row style={{padding: 0, width: '100%', margin: 0}}>
-          <Col style={{ width: '70%', marginLeft: '7%', padding: 20, backgroundImage: "linear-gradient(to right, #FFFFFF, #DFE9E8)", display: 'flex', alignItems: 'center'}} >
-          <h1 style={{color: "rgba(102, 102, 102, 1) ", fontSize: 70, textAlign: 'left'}}>
+        <Row style={{margin: 0, width: "100%"}}>
+          <Col style={{marginLeft: '7%', padding: 20, backgroundImage: "linear-gradient(to right, #FFFFFF, #DFE9E8)", display: 'flex', alignItems: 'center'}} >
+          <h1 style={{color: "rgba(102, 102, 102, 1) ", fontSize: 65, textAlign: 'initial'}}>
             Setor de {props.name} da APAE SJE
           </h1>
           </Col>
-          <Col style={{padding: 0, width: '30%'}}>
-            <Image src={sectionImage} fluid style={{width: '100%'}}/>           
+          <Col style={{padding: 0}}>
+            <Image src={sectionImage} style={{width: "100%"}} />           
           </Col>
         </Row>
         {subsection.map((section) => {
-          return (<Row>
-            <Col style={{padding: 20}}>Setor de {section.title}</Col>
-            <Col></Col>
+          return (
+          <Row >
+            <Col style={{padding: '0% 5%', color:  "rgba(102, 102, 102, 1)" }}>
+              <Row style={{marginTop: '25%'}}>
+                <Row style={{}}>
+                  <h1 style={{ fontSize: 60, fontWeight: '500' }}>
+                    {section.title}
+                  </h1>
+                </Row>
+                <Row style={{marginTop: "20%"}}>
+                  <h5 style={{ fontSize: 30, fontWeight: '400' }}>
+                    {section.subtitle}
+                  </h5>
+                </Row>
+              </Row>
+              <Row md={8} style={{marginTop: "20%"}}>
+                <p style={{ fontSize: 15, fontWeight: '300' }}>
+                  {section.text}
+                </p>
+              </Row>
+            </Col>
+            <Col md={5}>
+              <Image src={section.image} style={{}}/>
+            </Col>
           </Row>)
         })}
        
