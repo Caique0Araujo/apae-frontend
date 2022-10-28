6f4203cf58e970b80f3b42dc1effe83a0dd1244e
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2022-10-28 19:55:09 -0300

    tela principal publicação de pagina inteira
    falta a versão mobile ainda

---
+++ src/pages/home/components/full-page/full-page.jsx
@@ -0,0 +1,66 @@
+import './full-page.css'
+
+export default function FullPage(props){
+    return(
+        <nav className='fullPageBox'>
+            <div>
+                <img src="src\assets\images\pexels-mentatdgt-1336873.jpg" alt="" />
+            </div>
+            <div>
+                <h1>Título</h1>
+                <h2>Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque esse at 
+                    culpa aliquid totam et!
+                    Lorem ipsum dolor sit amet consectetur 
+                    adipisicing elit. Suscipit, corporis quidem. 
+                    Voluptatem, provident minima? Maxime 
+                    voluptate veritatis odit placeat dolore 
+                    animi illum, quaerat doloremque es
+                </h2>
+            </div>
+        </nav>
+    )
+}
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import Intro from './components/intro/intro';
 import Posts from './components/posts/posts';
+import FullPage from './components/full-page/full-page';
 import '../../css/DefaultStyle.min.css';
 import './home.css';
 export default function Home(){
@@ -9,6 +10,7 @@ export default function Home(){
             <Intro/>
             <div className='titleBox'><h1 className='title'>Últimos destaques</h1></div>
             <Posts arr={arr}/>
+            <FullPage/>
         </div>
         
     )
