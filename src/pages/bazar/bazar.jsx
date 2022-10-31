81b034a148354f11f15bf1c00a04fdb916256204
Matheus Ferreira <matheus.44300@gmail.com>
2022-10-31 20:54:15 -0300

    Added mobile style

--- src/pages/bazar/bazar.jsx
+++ src/pages/bazar/bazar.jsx
@@ -4,7 +4,8 @@ import MenuBarComponent from "../components/menu-bar-component/menu-bar-componen
 import OutlinedButtonComponent from "../components/outlined-button-component/outlined-button-component";
 import bazar from '../../assets/images/bazar.png';
 import '../../css/DefaultStyle.min.css';
-import './css/Bazar.min.css';
+import './css/bazar.min.css';
+import './css/bazar-mobile.min.css';
 import VariableWidthGrid from "react-variable-width-grid";
 import { ItemGridComponent } from "./components/item-grid-component";
 import FooterBaseComponent from "../components/footer-base-component/footer-base-component";
@@ -26,11 +27,12 @@ function Bazar() {
 
             <Container className='section-page'>
                 <Row>
-                    <Col>
-                        <img src={bazar}/>
+                    <Col className='logo-bazar'>
+                        <img className='logo-bazar' src={bazar}/>
                     </Col>
+                    
                     <Col className='title-col'>
-                        <h2 className='title right'>Conheça o bazar<br/>da APAE</h2>
+                        <h2 className='title right'>Conheça o bazar da APAE</h2>
                         <p className="subtitle right">Falar como funciona o bazar, como começou talvez. Falar como doar e como comprar. Accumsan eu ipsum nec, maximus rhoncus magna. Fusce cursus neque ullamcorper elit volutpat, id malesuada nulla dignissim. In </p>
                     
                         <Row className='row-card'>
@@ -43,36 +45,36 @@ function Bazar() {
 
             <Container className='section-page'>
                 <Row className="row-card">
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card">Como doar algo para o bazar?</h4>
                         <p className="description-card">Dizer aqui como deve ser feita a doação.<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
                     </Col>
 
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card right">O que posso doar?</h4>
                         <p className="description-card right">Dizer aqui quais tipos de coisas podem ser doadas.<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
                     </Col>
                 </Row>
 
                 <Row className="row-card">
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card">Qual o local para fazer a doação?</h4>
                         <p className="description-card">Dizer aqui o local para fazer a doação<br/>eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
                     </Col>
 
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card right">Qualquer outra informação relevante</h4>
                         <p className="description-card right">eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
                     </Col>
                 </Row>
 
                 <Row className="row-card">
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card">Qualquer outra informação relevante</h4>
                         <p className="description-card">eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum.</p>
                     </Col>
 
-                    <Col xs={4}>
+                    <Col>
                         <h4 className="title-card right">Qualquer outra informação relevante</h4>
                         <p className="description-card right">eu, porttitor cursus ligula. Fusce at lacus sit amet nisi aliquam finibus. Nunc luctus dignissim bibendum. </p>
                     </Col>
