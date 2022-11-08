8e1291b2fcd62c4faf1be6ca9bdf4deb8c3b4f22
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2022-11-08 17:38:24 -0300

    Paina de noticias adicionada

---
+++ src/pages/news/news.jsx
@@ -0,0 +1,53 @@
+import './css/news.min.css'
+
+export default function News(props) {
+    return(
+        <nav className='news-main-box'>
+            <div className='title-box'>
+                <h1>Exemplo de título da noticia na página propria </h1>
+            </div>
+            <img src="src\assets\images\pexels-mentatdgt-1336873.jpg" alt="" />
+            <div>
+                <h3>Exemplo de subtitulo para materia de pagina inteira</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                    Lorem ipsum dolor sit amet consectetur, 
+                    adipisicing elit. Culpa, suscipit ipsum? 
+                    Quisquam tempore, minima voluptatibus 
+                    repellat dicta optio dignissimos vero 
+                    sapiente sit, incidunt deleniti dolor 
+                    possimus quia sint itaque ipsa!
+                </p>
+            </div>
+             </nav>
+    )
+}
