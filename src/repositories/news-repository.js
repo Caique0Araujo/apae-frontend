fd808f838747cf18a1d27bffc16b6fc52697c75f
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-08 20:23:18 -0300

    Added settings.example.js

--- src/repositories/news-repository.js
+++ src/repositories/news-repository.js
@@ -1,5 +1,7 @@
+import { API_URL } from '../../settings.js';
+
 export async function getMostRecentNews(start, end) {
-    const result = await fetch(`http://localhost:3333/news/recents/${start}/${end}`);
+    const result = await fetch(`${API_URL}/news/recents/${start}/${end}`);
     if (result.status != 200) {
         return [];
     }
--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -1,5 +1,7 @@
+import { API_URL } from '../../settings.js';
+
 export async function getAllProducts() {
-    const result = await fetch('http://localhost:3333/products/all');
+    const result = await fetch(`${API_URL}/products/all`);
     if (result.status != 200) {
         return {
             statusCode: result.status,
