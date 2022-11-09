35a9f00a759504258edc1cb8f963f0edc265dca6
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2022-11-09 00:23:33 -0300

    menu adicionado

--- src/pages/components/menu-bar-component/menu-bar-component.jsx
+++ src/pages/components/menu-bar-component/menu-bar-component.jsx
@@ -1,6 +1,24 @@
+import './css/menu-bar.min.css'
+import OutlinedButtonComponent from '../outlined-button-component/outlined-button-component';
 function MenuBarComponent() {
     return (
-        <header>Menu</header>
+        <div className='header'>
+            <div>
+                <img src="src\assets\images\LOGO-APAE-BRASIL-01.png" alt="" />
+                <h4>APAE</h4>
+            </div>
+            <ul>
+                <li><a href="">Início</a></li>
+                <li><a href="">Saiba mais</a></li>
+                <li><a href="">Oficinas</a></li>
+                <li><a href="">Bazar</a></li>
+                <li><a href="">Setores</a></li>
+                <li><a href="">Quem somos</a></li>
+            </ul>
+            <div>
+                <button>Fale conosco</button>
+            </div>
+        </div>
     );
 }
 
