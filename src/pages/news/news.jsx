28415ef7035ed488ebc39b3d25fbf7ace4e5ddcb
Matheus Ferreira <matheus.44300@gmail.com>
2022-11-09 21:01:24 -0300

    Added route to show a news

--- src/pages/news/news.jsx
+++ src/pages/news/news.jsx
@@ -1,66 +1,70 @@
 import './css/news.min.css';
 import { useState } from 'react';
-import ModalProduct from '../bazar/components/modal-product/modal-product';
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Buffer } from 'buffer';
+import { getById } from '../../repositories/news-repository';
+import { FormattedDate, IntlProvider } from 'react-intl';
 
 export default function News(props) {
-    const [a, seta] = useState(false);
-    //let { id } = useParams();
-    //console.log(id);
+    const { id } = useParams();
+    const [news, setNews] = useState(null);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        loadNews();
+    }, []);
+
+    const loadNews = async() => {
+        try {
+            setLoading(true);
+            
+            const realValue = Buffer.from(id, 'base64').toString('utf-8');
+            const idNews = realValue.split('+')[0];
     
+            const _news = await getById(idNews);
+            setNews(_news);
+        } catch {
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    if (error == true) {
+        // TODO: Interessante colocar algo nesta parte
+        return (
+            <div>Ocorreu um error</div>
+        );
+    }
+
+    if (loading == true) {
+        // TODO: Interessante colocar algo nesta parte
+        return (
+            <div>Laoding</div>
+        );
+    }
+
+    if (news == null) {
+        // TODO: Interessante colocar algo nesta parte
+        return (
+            <div>News not founded</div>
+        );
+    }
+
     return(
         <nav className='news-main-box'>
             <div className='title-box'>
-                <h1>Exemplo de título da noticia na página propria </h1>
+                <h1>{news.title}</h1>
             </div>
-            <img src="src\assets\images\pexels-mentatdgt-1336873.jpg" alt="" />
+            <img src="../../src/assets/images/pexels-mentatdgt-1336873.jpg" alt="" />
             <div>
-                <h3>Exemplo de subtitulo para materia de pagina inteira</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                    Lorem ipsum dolor sit amet consectetur, 
-                    adipisicing elit. Culpa, suscipit ipsum? 
-                    Quisquam tempore, minima voluptatibus 
-                    repellat dicta optio dignissimos vero 
-                    sapiente sit, incidunt deleniti dolor 
-                    possimus quia sint itaque ipsa!
-                </p>
+                <p>{news.text}</p>
+                <IntlProvider locale='pt-BR'>
+                    <FormattedDate value={news.date} year="numeric" day="2-digit" month='long'/>
+                </IntlProvider>
             </div>
-            <button onClick={()=>{
-                seta(true)
-            }}>
-                teste
-            </button>
-
-            <ModalProduct show={a} onHide={() => seta(false)}/>
-             </nav>
+        </nav>
     )
 }
