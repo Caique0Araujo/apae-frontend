0e7c86184af05861cab0848119a7b2bd1c48395b
Paulo Fernando <61089612+Paulo-Fernando-R@users.noreply.github.com>
2022-11-16 22:23:51 -0300

    footer adiconado

--- src/pages/components/footer-base-component/footer-base-component.jsx
+++ src/pages/components/footer-base-component/footer-base-component.jsx
@@ -1,7 +1,23 @@
+import './css/footer.min.css'
 export default function FooterBaseComponent() {
     return (
         <footer>
-            <h3>Rodapé</h3>
+            <div>
+                <img src="src\assets\images\LOGO-APAE-BRASIL-01.png" alt="" />
+                <article>
+                    <h3>APAE - SJE 2022</h3>
+                    <h6>Administração 20xx - 20xx</h6>
+                </article>
+                
+            </div>
+            <div>
+                <h3>Este site foi desenvolvido por</h3>
+                <article>
+                    <h6>Caique Figueiredo de Araújo</h6>
+                    <h6>Matheus Ferreira Alves</h6>
+                    <h6>Paulo Fernando Rodrigues</h6>
+                </article>
+            </div>
         </footer>
     );
 }
