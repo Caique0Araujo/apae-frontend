92bb9b7fb2443b4c41c55f8e93a71036204aa61c
Matheus Ferreira <matheus.44300@gmail.com>
2022-12-12 20:40:19 -0300

    Updated

--- src/repositories/product-repository.js
+++ src/repositories/product-repository.js
@@ -1,7 +1,7 @@
 import { API_URL } from '../../settings.js';
 
 export async function getAllProducts() {
-    const result = await fetch(`${API_URL}/products/all`);
+    const result = await fetch(`${API_URL}/products/getAll`);
     if (result.status != 200) {
         return {
             statusCode: result.status,
