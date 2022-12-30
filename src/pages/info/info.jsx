eaa695558739c4df9858a5ec5bda98fe4b8f23f7
Caique0Araujo <caique.ara19@gmail.com>
2022-12-30 15:28:46 -0300

    Adjust info page text

--- src/pages/info/info.jsx
+++ src/pages/info/info.jsx
@@ -12,8 +12,8 @@ export default function Info() {
             <div className='main-header'>
                 <div className='main-content'>
                     <h2>APAE São João Evangelista</h2>
-                    <p>Ela oferta atendimento múltiplo – Escola e entidade assistencial, onde os alunos aprendem não só a educação formal, mas também a socialização e a independência.<br/>
-                    A APAE oferta atendimentos complementares como fisioterapia, psicologia, terapia ocupacional, fonoaudiologia e artes. 
+                    <p>É ofertado dentro da APAE atendimento múltiplo – Escola e entidade assistencial, onde os alunos aprendem não só a educação formal, mas também a socialização e a independência.<br/>
+                    A APAE também oferece atendimentos complementares como fisioterapia, psicologia, terapia ocupacional, fonoaudiologia e artes. 
                     </p>
                 </div>
             </div>
@@ -25,7 +25,7 @@ export default function Info() {
                 </div>
 
                 <div className='donation-itens'>
-                    <InfoDonationComponent icon={IconMoney} title={'Doação em dinheiro'} description={'Por meio de dinheiro vivo, diretamente na APAE/SJE; por carnê de doação; por meio de depósito; PIX; doação nos comércios, por meio do troco solidário. '}/>
+                    <InfoDonationComponent icon={IconMoney} title={'Doação em dinheiro'} description={'Por meio de dinheiro vivo, diretamente na APAE/SJE; Carnê de doação; Depósito; PIX; Doação nos comércios, através do troco solidário. '}/>
                     <InfoDonationComponent icon={IconClothes} title={'Doação de roupas e utensílios'} description={'Doação de peças básicas do vestuário; bolsas; sapatos; roupas de cama, mesa e banho; utensílios domésticos; ferramentas; materiais escolares; brinquedos e móveis. A APAE realiza a venda dos bens doados no BAZAR e a renda é revertida para a manutenção da instituição.'}/>
                     <InfoDonationComponent icon={IconPlasticBottle} title={'Doação de garrafas PET'} description={'Que são utilizadas para a confecção de vassouras.'}/>
                 </div>
